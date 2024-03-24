da59d433321db0ffa06e4e4675a1107b94d92963
ChoYoungSeung <choedcys@naver.com>
2024-03-24 09:58:23 +0900

    Initial Commit

---
+++ DataStructure-Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure-Algorithms
+//
+//  Created by 조영승 on 3/24/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
